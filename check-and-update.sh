81cc4c3f64393c8ead82735ae40a0cbd690ce96c
Wojciech Kozlowski <wk@wojciechkozlowski.eu>
2021-03-06 14:56:16 +0100

    Ignore intermediate versions

--- check-and-update.sh
+++ check-and-update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 SPEC_VERSION=`grep "Version:" mininet.spec | awk '{print $2}'`
-REMOTE_VERSION=`git ls-remote --tags https://github.com/mininet/mininet.git | awk '{print $2}' | grep -E "refs/tags/[0-9]+.[0-9]+.[0-9]+" | sort --version-sort | tail -n 1 | cut -d '/' -f 3`
+REMOTE_VERSION=`git ls-remote --tags https://github.com/mininet/mininet.git | awk '{print $2}' | grep -E "refs/tags/[0-9]+.[0-9]+.[0-9]+$" | sort --version-sort | tail -n 1 | cut -d '/' -f 3`
 
 if [ "${SPEC_VERSION}" = "${REMOTE_VERSION}" ]
 then
